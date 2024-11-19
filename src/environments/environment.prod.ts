f2158d2eed6e0d4d25d83b6f72c09e5331e325b9
VivianGomez <vn.gomez@uniandes.edu.co>
2024-11-19 17:50:39 -0500

    Nuevo gist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 const baseUrl =
- 'https://gist.githubusercontent.com/VivianGomez/cc4116c67063bed35e9a39723e7a9d6a/raw/f0029a4064817348f24f9b7bc250895a2de1bc25/anime.json';
+ 'https://gist.githubusercontent.com/VivianGomez/cc4116c67063bed35e9a39723e7a9d6a/raw/7699873b61ee4c4fde7da9f9ee8c4a125cbdac05/anime.json';
 
 export const environment = {
  production: false,
  baseUrl
 };
+
