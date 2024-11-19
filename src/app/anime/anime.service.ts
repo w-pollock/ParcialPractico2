13172c3c372841da73ea53c3e77743c4a3cd7a32
VivianGomez <vn.gomez@uniandes.edu.co>
2024-11-19 16:46:26 -0500

    Funcion base para el GetAnime con un id dado

--- src/app/anime/anime.service.ts
+++ src/app/anime/anime.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Anime } from './anime';
 
 @Injectable({
@@ -15,6 +15,15 @@ export class AnimeService {
 
   getAnimes(): Observable<Anime[]> {
     return this.http.get<Anime[]>(this.apiUrl);
+  }
+
+  getAnime(id: string): Observable<Anime> {
+    return this.http.get<Anime[]>(this.apiUrl).pipe(
+      map((animes: Anime[]) => {
+        //Complete con el código necesario para recorrer los animes y retornar el anime con el id buscado
+        throw new Error(`Anime con ID ${id} no encontrado`);
+      })
+    );
   }
 
 }
